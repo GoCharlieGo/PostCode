54bcfda23b28d991f9aa1e6f073faeb695fe1c52
GoCharlieGo <sonikx12@mail.ru>
2015-01-10 14:04:50 +0300

    Master Project

---
+++ PostCode/PostCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PostCode.Startup))]
+namespace PostCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
