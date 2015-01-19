f26092ad123bafe4945e0116b71fa5351c203f83
CaptainDukalis <vadimchik_18@mail.ru>
2015-01-19 16:52:53 +0300

    commit Lucene/SearchByPost.cs

--- PostCode/PostCode/Controllers/AccountController.cs
+++ PostCode/PostCode/Controllers/AccountController.cs
@@ -240,12 +240,12 @@ namespace PostCode.Controllers
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
                 string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                if (Request.Url != null)
-                {
-                    var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                }
-                return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                //if (Request.Url != null)
+                //{
+                //    var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                //    await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                //}
+                //return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
             // If we got this far, something failed, redisplay form
